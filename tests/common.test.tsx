846a63beb506c02af01a45d2f38ccef8c3369fc3
Shrikant Dhayaje <shrikantdhayaje@gmail.com>
2023-09-28 21:03:41 +0530

    updated the scripts

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
 
-import { GoogleAdUnit } from '../src';
+import GoogleAdUnit from '../src';
 
 describe('Common render', () => {
   it('renders without crashing', () => {
