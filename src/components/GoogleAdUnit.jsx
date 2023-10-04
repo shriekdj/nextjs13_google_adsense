48ebd02f3d88624c4a6d2a8dee506b9daabd72a4
Shrikant Dhayaje <shrikantdhayaje@gmail.com>
2023-10-04 20:05:44 +0530

    added try except

--- src/components/GoogleAdUnit.jsx
+++ src/components/GoogleAdUnit.jsx
@@ -2,24 +2,14 @@
 import React from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-type GoogleAdUnitProps = {
-  children: React.ReactNode;
-};
-
-declare global {
-  interface Window {
-    adsbygoogle?: any | any[];
-  }
-}
-
-const GoogleAdUnit: React.FC<GoogleAdUnitProps> = ({ children }) => {
+const GoogleAdUnit = ({ children }) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   React.useEffect(() => {
     try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
     } catch(err) {
-      console.error(err)
+        console.error(err)
     }
   }, [pathname, searchParams]);
   return <React.Fragment>{children}</React.Fragment>;
