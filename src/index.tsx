94847736556c737258ea94e7bf2c9391eb74babb
Shrikant Dhayaje <shrikantdhayaje@gmail.com>
2023-10-04 20:29:50 +0530

    suspense code added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
-import { GoogleAdUnitProps } from './GoogleAdUnitClient';
+import GoogleAdUnitClient, { GoogleAdUnitProps } from './GoogleAdUnitClient';
 
-const GoogleAdUnitClient = React.lazy(() => import('./GoogleAdUnitClient'))
+// const GoogleAdUnitClient = React.lazy(() => import('./GoogleAdUnitClient'))
 
 const GoogleAdUnit = ({ children }: GoogleAdUnitProps) => {
     return <Suspense fallback={null}>
