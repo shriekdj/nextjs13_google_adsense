ffc8199e9a9b0a44f7953a85b472062f6c19ba1f
Shrikant Dhayaje <shrikantdhayaje@gmail.com>
2023-10-04 20:30:36 +0530

    formated code

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import GoogleAdUnitClient, { GoogleAdUnitProps } from './GoogleAdUnitClient';
 // const GoogleAdUnitClient = React.lazy(() => import('./GoogleAdUnitClient'))
 
 const GoogleAdUnit = ({ children }: GoogleAdUnitProps) => {
-    return <Suspense fallback={null}>
-        <GoogleAdUnitClient>{children}</GoogleAdUnitClient>
+  return (
+    <Suspense fallback={null}>
+      <GoogleAdUnitClient>{children}</GoogleAdUnitClient>
     </Suspense>
-}
+  );
+};
 
-export default GoogleAdUnit
+export default GoogleAdUnit;
